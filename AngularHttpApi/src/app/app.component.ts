41c85b4da6c04952a48d2e76a9b5e539e0821f58
AlaminRonya <hprony12@gmail.com>
2023-01-15 13:36:03 +0600

    insert user data api

--- AngularHttpApi/src/app/app.component.ts
+++ AngularHttpApi/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { User } from './interfaces/user';
 import { UserService } from './service/user.service';
 
 @Component({
@@ -7,8 +8,9 @@ import { UserService } from './service/user.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterContentInit{
   title = 'AngularHttpApi';
+
 
   // constructor(){
   //   type HttpResponse = {code: number, data: string};
@@ -40,12 +42,45 @@ export class AppComponent implements OnInit{
   // }
 
 
+  private user: User = {
+    'name': 'Md. Al Amin Islam Rony',
+    'username': 'rony',
+    'email': 'alamin@gmail.com',
+    'address': {
+      'street': 'Kulas Light',
+      'suite': 'Apt. 556',
+      'city': 'Gwenborough',
+      'zipcode': '92998-3874',
+      'geo': {
+        'lat': '-37.3159',
+        'lng': '81.1496'
+      }
+    },
+    'phone': '1-770-736-8031 x56442',
+    'website': 'hildegard.org',
+    'company': {
+      'name': 'Romaguera-Crona',
+      'catchPhrase': 'Multi-layered client-server neural-net',
+      'bs': 'harness real-time e-markets'
+    }
+  }
+
   constructor(private userService: UserService){
   }
   ngOnInit(): void {
+    this.onCreateUser();
+    console.log('Timeout.....................');
+    setTimeout(()=>{
+          console.log('Timeout.....................');
+    }, 3*1000);
+    
+  }
+  ngAfterContentInit(): void {
     this.onGetUsers();
     this.onGetUser();
   }
+
+
 
   onGetUsers(): void {
     this.userService.getUsers().subscribe(
@@ -62,5 +97,16 @@ export class AppComponent implements OnInit{
       ()=> console.log("Done getting user...........")
     );
   };
+
+  onCreateUser(): void {
+    this.userService.createUser(this.user).subscribe(
+      (response) => {console.log(response)},
+      (error?: any) => console.log('Bad request=====>',error),
+      ()=> console.log("created user...........")
+    );
+  };
+
+
+
 
 }
