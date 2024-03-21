0fa8189c99ef5aee1805bf72a8ef85548f40afce
AlaminRonya <hprony12@gmail.com>
2024-03-21 22:57:48 +0600

    Jira Layout Design

--- jira-app/src/app/app.module.ts
+++ jira-app/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { BoardComponent } from './pages/board/board.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { UsersComponent } from './pages/users/users.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    BoardComponent,
+    LoginComponent,
+    ProjectsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
